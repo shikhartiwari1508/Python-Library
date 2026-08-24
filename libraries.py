5f70e957bcf358e65f564f7b5f10f916c9894634
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-08-24 21:48:57 +0530

    Python Libraries

--- libraries.py
+++ libraries.py
@@ -32,3 +32,17 @@ from scipy import stats
 data = [10,20,30,40,50]
 print("Mean :",stats.tmean(data))
 print("Varience :",stats.tvar(data))
+
+
+#SEABORN PROGRAMME :
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+df=pd.DataFrame({
+    "Name":["Shikhar","Shivam","Saurabh","Shivansh","Suryansh","Shivesh"],
+    "Marks":[80, 58, 68, 95, 74, 55],
+    "Address":["Pratapgarh","Ballia","Kunda","Prayagraj","Allahabad","Rewa"]
+})
+sns.barplot(data=df,x="Name",y="Marks")
+plt.show()
